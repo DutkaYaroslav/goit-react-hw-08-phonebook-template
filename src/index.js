d1223ae4d346f2c96bbba1bbd0c84ad8dd9e8cba
DutkaYaroslav <yaroslavdutka@gmail.com>
2023-02-12 12:59:32 +0200

    1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import AuthContextProvider from 'components/store/Context';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter basename="goit-react-hw-08-phonebook-template">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </AuthContextProvider>
